768b0394e25bccc3db35508d86655fffa6f873dc
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2022-11-21 22:38:02 +0100

    Testnummer entfernt

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var rm = document.querySelector('#form-input-rm')
 var zlb = document.querySelector('#form-input-zlb')
 
 form.addEventListener('change', function() {
-    form.action = "sms:017663003775?body=RM: " + rm.value + "\nZLB: " + zlb.value;
+    form.action = "sms:?body=RM: " + rm.value + "\nZLB: " + zlb.value;
 });
 
 
