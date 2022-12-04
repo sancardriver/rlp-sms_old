7c09abd15c8c148d51895837ea3ee941948d2ad9
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2022-12-04 03:13:40 +0100

    test

--- js/main.js
+++ js/main.js
@@ -43,21 +43,27 @@ async function webShare() {
     }
 }
 
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
 
 function onLoad() {
-
-    const forms = document.querySelectorAll('.needs-validation')
-
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-            form.classList.add('was-validated')
-        }, false)
-    })
-
 
     //document.querySelector('#share').addEventListener('click', webShare);
 
