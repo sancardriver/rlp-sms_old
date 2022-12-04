19549cea84bf5e2e8996d194abd606e79b2295ad
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2022-12-04 04:07:02 +0100

    test

--- js/main.js
+++ js/main.js
@@ -75,3 +75,22 @@ function onLoad() {
 })()
 
 window.addEventListener('load', onLoad);
+
+
+$('#form-input-birth').datepicker("option", "onSelect", function(dateText, inst) {
+    var dob = $(this).datepicker('getDate');
+    var age = GetAge(dob);
+    if (age >= 16 && age < 18) {
+        alert("The minimum age requirement for supplementary card applicant is 18 years old. For applicant aged 16 and 17, and are going overseas to study, please submit the letter of acceptance from the education institution.");
+    }
+});
+
+function GetAge(birthDate) {
+    var today = new Date();
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
