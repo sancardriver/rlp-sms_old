99c1a8a67cef3d79ef2c0f38266f6f198ad6d999
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2022-12-04 03:05:13 +0100

    versuch

--- js/main.js
+++ js/main.js
@@ -55,9 +55,7 @@ function onLoad() {
                 event.stopPropagation()
             }
             form.classList.add('was-validated')
-            if (!form.checkValidity()) {
-                webShare();
-            }
+            webShare();
         }, false)
     })
 
